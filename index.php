82819e437cca787b0f40a84e484ae6b1be4a17b1
Raimund Ege <ege@niu.edu>
2018-04-19 11:45:17 -0500

    first commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+        <title>Laravel PHP Webapplication Showcase</title>
+    </head>
+    <body>
+
+        <div class="container">
+            <h1>Laravel PHP Webapplication Showcase</h1>
+
+            <ul class="nav nav-tabs" id="myTab" role="tablist">
+                <li class="nav-item">
+                    <a class="nav-link active" id="home-tab" data-toggle="tab" href="#one" role="tab">Example 1</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="profile-tab" data-toggle="tab" href="#two" role="tab">Example 2</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" id="contact-tab" data-toggle="tab" href="#three" role="tab">Example 3</a>
+                </li>
+            </ul>
+            <div class="tab-content" id="myTabContent">
+                <div class="tab-pane fade show active" id="one" role="tabpanel">
+                    <iframe src=one height="800" width="90%"></iframe>
+                    <a class="btn btn-primary fixed-bottom" href="one" role="button" target="_">Visit Site</a>
+                </div>
+                <div class="tab-pane fade" id="two" role="tabpanel">
+                    <iframe src="//www.ege.com" height="800" width="90%"></iframe>
+                    <a class="btn btn-primary fixed-bottom" href="//www.ege.com" role="button" target="_">Visit Site</a>
+                </div>
+                <div class="tab-pane fade" id="three" role="tabpanel">
+                    ...
+                </div>
+            </div>               
+        </div>
+
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+    </body>
+</html>
